ab836dd4cdd458a219e184b378763e20c5882615
eminkin97 <eminkin99@gmail.com>
2018-08-04 21:40:08 -0400

    Styling changes on stage select screen

--- components/StageSelect.js
+++ components/StageSelect.js
@@ -11,24 +11,33 @@ export default class StageSelect extends React.Component {
   }
   render() {
     return (
-	<ScrollView directionalLockEnabled={false}>
+	<ScrollView>
+		<View style={styles.container}>
 		{
 			level_meta_data.maps.map(stage =>	
 				<TouchableHighlight style={[styles.stage_blocks, { backgroundColor: stage.color }]}
+					key={stage.num}
 					onPress={() => this.stagePress(stage.title, stage.dir, stage.color)}>
 					<Text style={styles.stage_text}>{ stage.title }</Text>
 				</TouchableHighlight>
 			)
 		}
+		</View>
 	</ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 0,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		justifyContent: 'flex-start'
+	},
 	stage_blocks: {
-		width: 100,
-		height: 100,
+		minWidth: 100,
+		height: 125,
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center'
